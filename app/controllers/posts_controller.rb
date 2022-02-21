d5362e7f5da5e34b63d851abc57c23ffa387b79a
tsuchiya-yu2 <yuji2tsuchiya@gmail.com>
2022-02-22 00:28:38 +0900

    [Fix] Postモデル・コントローラーのLintを崩す

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,48 +1,56 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: %i[edit update destroy]
+
+
+before_action :set_post, only: %i[edit update destroy]
 
   def index
     @posts = Post.all
   end
 
-  def new
-    @post = Post.new
-  end
 
-  def create
-    @post = Post.new(post_params)
 
-    if @post.save
-      redirect_to posts_path, notice: 'Create Post.'
-    else
-      flash.now[:danger] = 'Error: Not Create Post.'
-      render :new
+    def new
+      @post = Post.new
     end
-  end
+      def create
+        @post = Post.new(post_params)
 
-  def edit; end
+        if @post.save
+          redirect_to posts_path, notice: 'Create Post.'
+        else
+          flash.now[:danger] = 'Error: Not Create Post.'
+          render :new
+        end
+      end
 
-  def update
-    if @post.update(post_params)
-      redirect_to posts_path, success: 'Update Post.'
-    else
-      flash.now[:danger] = 'Error: Not Update Post.'
-      render :edit
+
+
+    def edit
     end
-  end
+      def update
+        if @post.update(post_params)
+          redirect_to posts_path, success: 'Update Post.'
+        else
+          flash.now[:danger] = 'Error: Not Update Post.'
+          render :edit
+        end
+      end
 
-  def destroy
-    @post.destroy!
-    redirect_to posts_path, success: 'Delete Post.'
-  end
 
-  private
 
-  def set_post
-    @post = Post.find(params[:id])
-  end
+        def destroy
+          @post.destroy!
+          redirect_to posts_path, success: 'Delete Post.'
+        end
 
-  def post_params
-    params.require(:post).permit(:title, :body)
-  end
+
+
+private
+def set_post
+  @post = Post.find(params[:id])
+end
+
+def post_params
+  params.require(:post).permit(:title, :body)
+end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ApplicationRecord
-  validates :title, presence: true, length: { in: 2..255 }
-  validates :body,  presence: true, length: { in: 2..500 }
+  validates :title, presence:true, length: { in: 2..255 }
+  validates :body,  presence: true, length:{ in: 2..500 }
 end
