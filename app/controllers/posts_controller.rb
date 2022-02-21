1be83895c377e25182b6a6b3b2ae16cc6d396d68
tsuchiya-yu2 <yuji2tsuchiya@gmail.com>
2022-02-22 00:43:30 +0900

    [Fix] 自動補完機能を配してLintを崩す

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,56 +1,49 @@
 class PostsController < ApplicationController
-
-
-before_action :set_post, only: %i[edit update destroy]
+ before_action :set_post, only: %i[edit  update  destroy]
 
   def index
     @posts = Post.all
   end
 
+  def new
+    @post = Post.new  
+  end
 
+  def create
+    @post = Post.new(post_params)
 
-    def new
-      @post = Post.new
+   if @post.save
+      redirect_to posts_path, notice: 'Create Post.'
+     else
+      flash.now[:danger] = 'Error: Not Create Post.'
+       render :new
     end
-      def create
-        @post = Post.new(post_params)
+  end
 
-        if @post.save
-          redirect_to posts_path, notice: 'Create Post.'
-        else
-          flash.now[:danger] = 'Error: Not Create Post.'
-          render :new
-        end
+
+
+  def edit
+  end
+  def update
+      if @post.update(post_params)
+      redirect_to posts_path, success: 'Update Post.'
+      else
+      flash.now[:danger] = 'Error: Not Update Post.'
+      render :edit
       end
-
-
-
-    def edit
     end
-      def update
-        if @post.update(post_params)
-          redirect_to posts_path, success: 'Update Post.'
-        else
-          flash.now[:danger] = 'Error: Not Update Post.'
-          render :edit
-        end
-      end
-
-
-
-        def destroy
-          @post.destroy!
-          redirect_to posts_path, success: 'Delete Post.'
-        end
+  def destroy
+   @post.destroy!
+   redirect_to posts_path, success: 'Delete Post.'
+  end
 
 
 
 private
-def set_post
-  @post = Post.find(params[:id])
-end
-
-def post_params
+  def set_post
+   @post = Post.find(params[:id])
+  end
+  def post_params
   params.require(:post).permit(:title, :body)
-end
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
-class Post < ApplicationRecord
+class Post < ApplicationRecord  
   validates :title, presence:true, length: { in: 2..255 }
   validates :body,  presence: true, length:{ in: 2..500 }
 end
+
+
+  
